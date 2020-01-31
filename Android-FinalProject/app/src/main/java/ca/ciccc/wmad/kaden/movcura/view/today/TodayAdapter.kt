54ca8a084f4d1661cf81eb44946cd4f9e56cec03
kaden-kykim <kaden.kykim@gmail.com>
2020-01-31 05:10:33 -0800

    One-click toggle complete

--- Android-FinalProject/app/src/main/java/ca/ciccc/wmad/kaden/movcura/view/today/TodayAdapter.kt
+++ Android-FinalProject/app/src/main/java/ca/ciccc/wmad/kaden/movcura/view/today/TodayAdapter.kt
@@ -1,29 +1,66 @@
 package ca.ciccc.wmad.kaden.movcura.view.today
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ca.ciccc.wmad.kaden.movcura.data.SimpleMovieData
 import ca.ciccc.wmad.kaden.movcura.databinding.ListViewTodayMovieBinding
+import kotlinx.android.synthetic.main.list_view_today_movie.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class TodayAdapter :
+class TodayAdapter(
+    private val onCheckedChangeListener: OnCheckedChangeListener,
+    private val onClickListener: OnClickListener
+) :
     ListAdapter<SimpleMovieData, TodayAdapter.SimpleMovieDataViewHolder>(SimpleMovieDataDiffCallback()) {
 
     private val adapterScope = CoroutineScope(Dispatchers.Default)
 
+    private var tmpCover: FrameLayout? = null
+    private var tmpPosition: Int = -1
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleMovieDataViewHolder {
+        tmpPosition = -1
+        tmpCover = null
         return SimpleMovieDataViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: SimpleMovieDataViewHolder, position: Int) {
         val simpleMovieData = getItem(position) as SimpleMovieData
+        holder.itemView.setOnClickListener {
+            if (it.layout_list_view_movie_cover.visibility == View.GONE) {
+                initPosterCover()
+                it.layout_list_view_movie_cover.visibility = View.VISIBLE
+                tmpCover = it.layout_list_view_movie_cover
+                tmpPosition = position
+                onClickListener.onClick(null)
+            } else {
+                onClickListener.onClick(simpleMovieData)
+            }
+        }
+        holder.itemView.toggleButton_favorite.setOnCheckedChangeListener { _, isChecked ->
+            onCheckedChangeListener.onCheckedChanged(simpleMovieData, isChecked)
+        }
+        if (tmpPosition == position) {
+            holder.itemView.layout_list_view_movie_cover.visibility = View.VISIBLE
+        } else {
+            holder.itemView.layout_list_view_movie_cover.visibility = View.GONE
+        }
+
         holder.bind(simpleMovieData)
+    }
+
+    fun initPosterCover() {
+        tmpCover?.visibility = View.GONE
+        tmpPosition = -1
+        tmpCover = null
     }
 
     fun submitSimpleMovieList(list: List<SimpleMovieData>) {
@@ -49,6 +86,18 @@ class TodayAdapter :
                 )
             }
         }
+    }
+
+    class OnClickListener(val clickListener: (simpleMovieData: SimpleMovieData?) -> Unit) {
+        fun onClick(simpleMovieData: SimpleMovieData?) = clickListener(simpleMovieData)
+    }
+
+    class OnCheckedChangeListener(
+        val checkedChangeListener:
+            (SimpleMovieData: SimpleMovieData, isChecked: Boolean) -> Unit
+    ) {
+        fun onCheckedChanged(simpleMovieData: SimpleMovieData, isChecked: Boolean) =
+            checkedChangeListener(simpleMovieData, isChecked)
     }
 }
 
--- Android-FinalProject/app/src/main/java/ca/ciccc/wmad/kaden/movcura/view/today/TodayFragment.kt
+++ Android-FinalProject/app/src/main/java/ca/ciccc/wmad/kaden/movcura/view/today/TodayFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +13,10 @@ import ca.ciccc.wmad.kaden.movcura.database.favorite.FavoriteDB
 import ca.ciccc.wmad.kaden.movcura.databinding.FragmentTodayBinding
 
 class TodayFragment : Fragment() {
+
+    lateinit var nowPlayingAdapter: TodayAdapter
+    lateinit var upcomingAdapter: TodayAdapter
+    lateinit var trendAdapter: TodayAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +32,49 @@ class TodayFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        val nowPlayingAdapter = TodayAdapter()
-        val upcomingAdapter = TodayAdapter()
-        val trendAdapter = TodayAdapter()
+        nowPlayingAdapter = TodayAdapter(
+            TodayAdapter.OnCheckedChangeListener { data, isChecked ->
+                Toast.makeText(context, "Toggle: $isChecked", Toast.LENGTH_SHORT).show()
+            },
+            TodayAdapter.OnClickListener {
+                it?.let {
+                    Toast.makeText(context, "Click on ${it.title}", Toast.LENGTH_SHORT).show()
+                    nowPlayingAdapter.initPosterCover()
+                }
+                it?:let {
+                    upcomingAdapter.initPosterCover()
+                    trendAdapter.initPosterCover()
+                }
+
+            })
+        upcomingAdapter = TodayAdapter(
+            TodayAdapter.OnCheckedChangeListener { data, isChecked ->
+
+            },
+            TodayAdapter.OnClickListener {
+                it?.let {
+                    Toast.makeText(context, "Click on ${it.title}", Toast.LENGTH_SHORT).show()
+                    upcomingAdapter.initPosterCover()
+                }
+                it?:let {
+                    nowPlayingAdapter.initPosterCover()
+                    trendAdapter.initPosterCover()
+                }
+            })
+        trendAdapter = TodayAdapter(
+            TodayAdapter.OnCheckedChangeListener { data, isChecked ->
+
+            },
+            TodayAdapter.OnClickListener {
+                it?.let {
+                    Toast.makeText(context, "Click on ${it.title}", Toast.LENGTH_SHORT).show()
+                    trendAdapter.initPosterCover()
+                }
+                it?:let {
+                    nowPlayingAdapter.initPosterCover()
+                    upcomingAdapter.initPosterCover()
+                }
+            })
 
         binding.layoutTodayNowPlaying.layoutTodayMovieList.adapter = nowPlayingAdapter
         binding.layoutTodayUpcoming.layoutTodayMovieList.adapter = upcomingAdapter
